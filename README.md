84b38a57184e175e148cc977b8a4431af60453da
Tim Farland <twfarland@gmail.com>
2016-06-19 16:35:32 +1200

    dift

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ecto-vdom
 virtual dom library
+
+I'm still experimenting with this. Not fit for usage.
--- src/vdom.js
+++ src/vdom.js
@@ -1,28 +1,30 @@
+var dift = require('dift')
+
+
 // Js types
-var UNDEFINED = 'undefined'
-var STRING = 'string'
-var NUMBER = 'number'
-var FUNCTION = 'function'
-var OBJECT = '[object Object]'
+var STRING          = 'string'
+var NUMBER          = 'number'
+var FUNCTION        = 'function'
 
 // Props
-var KEY = 'key'
-var VALUE = 'value'
-var CHECKED = 'checked'
-var SELECTED = 'selected'
-var FOCUS = 'focus'
-var ON = 'on'
+var KEY             = 'key'
+var VALUE           = 'value'
+var CHECKED         = 'checked'
+var SELECTED        = 'selected'
+var DISABLED        = 'disabled'
+var FOCUS           = 'focus'
+var ON              = 'on'
+var EMPTYSTRING     = ''
 
 // Html
-var FIELD_ELEMENTS = { input:1, select:1, textarea:1 }
-var VOID_ELEMENTS = { area:1, base:1, br:1, col:1, command:1, embed:1, hr:1, img:1, input:1, keygen:1, link:1, meta:1, param:1, source:1, track:1, wbr:1 };
+var VOID_ELEMENTS = { area:1, base:1, br:1, col:1, command:1, embed:1, hr:1, img:1, input:1, keygen:1, link:1, meta:1, param:1, source:1, track:1, wbr:1 }
 
 // vDomType
-var VATOM = 0
-var VNODE = 1
-var VCHILD = 2
-var VRENDERED = 3
-var VNULL = 4
+var VATOM   = 0
+var VNODE   = 1
+var VCHILD  = 2
+var VNULL   = 3
+
 
 /*
 vAtom :: 
@@ -35,52 +37,22 @@ vNode ::
     [String, { attrs }, [vDom ...]] 
 
 vChild ::
-    [Function, { params } ...]
-
-vRendered ::
-    [true, vChild, vDom]    
+    [Function, { params }, vNode?]
 
 vDom :: 
     vAtom
     vNode
     vChild
-    vRendered
     vNull
 */
 
 
-/// ---------- UTILS ----------
-
-var slice = Array.prototype.slice
-var toString = Object.prototype.toString
-
-
-try {
-    slice.call(document.body.childNodes)
-
-} catch (e) {
-    slice = function () {
-        var res = [], i
-        for (i = 0; i < this.length; i++) { 
-        	res.push(this[i]) 
-        }
-        return res  
-    }
-}
-
-
 // vDom -> vDomType
 function getType (vDom) {
+
     if (vDom instanceof Array) {
-        if (typeof vDom[0] === FUNCTION) {
-            return VCHILD
+        return typeof vDom[0] === FUNCTION ? VCHILD : VNODE
 
-        } else if (vDom[0] === true) {
-            return VRENDERED
-
-        } else {    
-            return VNODE
-        }
     } else if (typeof vDom === STRING || typeof vDom === NUMBER) {
         return VATOM
 
@@ -90,329 +62,216 @@ function getType (vDom) {
 }
 
 
-// A -> Bool
-function truthy (n) {
-	if (n instanceof Array || typeof n === STRING) return n.length > 0
-    return !!(n === 0 || n)
+// vDom -> int -> key
+function keyOf (v, i) {
+    return (v instanceof Array && v[1] && v[1].key) || i
 }
 
 
-// VDom -> Bool
-function vDomIsKeyed (vDom) {
-	return vDom[2] && vDom[2][0] && vDom[2][0][1] && typeof vDom[2][0][1].key !== 'undefined'
+// [vDom] -> [{ key: *, vDom: vDom, pos: i }]
+function groupChildren (vDoms) {
+    var res = []
+    var v
+    var i
+    for (i = 0; i < vDoms.length; i++) {
+        v = vDoms[i]
+        if (!(v === false || v === undefined || v === null)) {
+            res.push({ key: keyOf(v, i), vDom: v, pos: i })
+        }
+    }
+    return res
 }
 
 
-// Node -> Int -> Node -> _
-// inserts the child in the parent at index
-function insertBeforeIndex (parent, i, child) { 
+// vDom -> key
+function getKey (v) { 
+    return v.key 
+}
 
-    var next = parent.childNodes[i]
 
-    if (next) {
-        parent.insertBefore(child, next)
-    } else {
-        parent.appendChild(child)
+// [vDom] -> [vDom] -> domNode -> _
+function updateChildren (prevChildren, nextChildren, D) {
+
+    dift.default(
+
+        groupChildren(prevChildren),
+        groupChildren(nextChildren),
+
+        function effect (type, current, next, pos) {
+
+            switch (type){
+                case dift.CREATE: // null, new, posToCreate
+                    D.insertBefore(vDomToDom(next.vDom), D.childNodes[pos] || null)
+                    break
+
+                case dift.UPDATE: // old, new, null
+                    updateDom(current.vDom, next.vDom, D.childNodes[current.pos], D)
+                    break
+
+                case dift.MOVE: // old, new, newPos
+                    D.insertBefore(D.childNodes[current.pos], D.childNodes[pos])
+                    updateDom(current.vDom, next.vDom, D.childNodes[current.pos], D)
+                    break
+
+                case dift.REMOVE: // null, old, null
+                    D.removeChild(D.childNodes[current.pos])
+                    break
+            }
+        },
+
+        getKey
+    )
+}
+
+
+// vDom -> vDom -> domNode -> domNode _
+function updateDom (current, next, D, DParent) {
+
+    if (D === undefined) { throw new Error('No dom node to update') }
+
+    if (current === next) { return }
+    
+    var currentExists = current !== undefined
+    var nextExists    = next !== undefined
+
+    if (!currentExists && nextExists) {
+        DParent.appendChild(vDomToDom(next))
+
+    } else if (currentExists && !nextExists) {
+        DParent.removeChild(D)
+
+    } else if (currentExists && nextExists) {
+
+        var currentType   = getType(current)
+        var nextType      = getType(next)
+
+        if (shouldExploreFurther(current, next, currentType, nextType)) {
+
+            switch (nextType) {
+
+                case VNODE:
+                    updateAttributes(current[1], next[1], D)
+                    updateChildren(current[2], next[2], D)
+                    break
+
+                case VCHILD:
+                    if (!next[0].shouldUpdate || next[0].shouldUpdate(current[1], next[1])) {
+                        next[2] = next[0](next[1])
+                        updateAttributes(current[2][1], next[2][1], D)
+                        updateChildren(current[2][2], next[2][2], D)
+
+                    } else {
+                        next[2] = current[2]
+                    }
+                    break
+            }
+
+        } else if (current !== next) {
+            DParent.replaceChild(vDomToDom(next), D)
+        }
     }
 }
 
 
+// vDom -> vDom -> domNode -> _
+function updateAttributes (currentAttrs, nextAttrs, D) {
 
-// --------- UPDATE ----------
-
-
-// vChild -> _
-// Mutates A into vRendered
-function renderChild (vChild) {
-    var vDom = vChild[0].apply(null, vChild.slice(1))
-    var vChildClone = vChild.slice()
-    vChild.length = 0
-    vChild.push(true)
-    vChild.push(vChildClone)
-    vChild.push(vDom)
-}
-
-
-// vRendered -> vChild -> _
-// mutates B, so it has same contents as A
-function transferRendered (A, B) {
-    B.length = 0
-    for (var i = 0; i < A.length; i++) {
-        B.push(A[i])
-    }
-}
-
-
-// Node -> VDom -> VDom -> _
-// A and B should only ever be the same Tag or Child (rendered or unrendered)
-function updateDom (D, A, B) {
-
-    switch (getType(A)) {
-
-        case VCHILD:
-
-            renderChild(A) // mutates A
-            updateDom(D, A, B)
-
-            break
-
-        case VRENDERED:
-
-            var vChildA = A[1]
-            var vDomA   = A[2]
-            var fA      = vChildA[0]
-
-            if (!fA.shouldUpdate || fA.shouldUpdate(vChildA.slice(1), vChildB.slice(1))) {
-
-                transferRendered(A, B) // transfer A to B, so it becomes rendered without execution
-
-            } else {
-                // render B
-                renderChild(B) // mutates B
-
-                updateDom(D, vDomA, B[2])
-            }
-
-            break
-                
-
-        case VNODE:
-
-            if (A[1] || B[1]) {
-                (FIELD_ELEMENTS[D.tagName.toLowerCase()] ? 
-                    updateFieldAttributes : 
-                    updateAttributes)(D, A[1] || {}, B[1] || {})
-
-                updateEvents(D, (A[1] || {})[ON], (B[1] || {})[ON])
-            }
-
-            if (A[2] || B[2]) {
-                (vDomIsKeyed(B) ? 
-                    updateChildrenKeyed : 
-                    updateChildrenPairwise)(D, slice.call(D.childNodes), A[2] || [], B[2] || [])
-            }
-
-            break
-
-        default:
-            return
-    }
-}
-
-
-// Node -> { EventName: Handler } -> { EventName: Handler } -> _
-// just clear all events on the node and add the new ones
-// perhaps a bit rough, but let's see for now
-// in practice there should be only one or a couple handlers on a node
-function updateEvents (D, AEvts, BEvts) {
-
+    var a
+    var currentVal
+    var nextVal
     var evt
+    var currentEvts = currentAttrs[ON]
+    var nextEvts    = nextEvts[ON]
 
-    if (AEvts) {
-        for (evt in AEvts) {
-            D.removeEventListener(evt, AEvts[evt])
-        }
-    }
+    for (a in currentAttrs) { // remove all those not in B from A
 
-    if (BEvts) {
-        for (evt in BEvts) {
-            D.addEventListener(evt, BEvts[evt])
-        }
-    }
-}
+        currentVal = currentAttrs[a]
+        nextVal = nextAttrs[a]
 
+        if (nextVal === undefined || nextVal === null || nextVal === false) {
 
-
-// Node -> Attrs -> Attrs -> _
-// Update the attributes from the new VDom (b) in the old Dom (b) by comparing 
-// with attributes of the current VDom (a).
-// If it's in a but not in b, it should be removed from d.
-// If it's in b, it should be set in d.
-function updateAttributes (D, AAttrs, BAttrs) {
-
-	var AAttr, BAttr
-
-    for (AAttr in AAttrs) {
-        if (AAttr !== KEY && AAttr !== ON && !truthy(BAttrs[AAttr])) {
-        	D.removeAttribute(BAttrs[AAttr])
+            switch (a) {
+                case ON:
+                case KEY:
+                    break
+                case CHECKED:
+                case DISABLED:
+                case SELECTED:
+                    D[a] = false
+                    break
+                case VALUE:
+                    D.value = EMPTYSTRING
+                    break
+                case FOCUS:
+                    D.blur()
+                    break
+                default:
+                    D.removeAttribute(a)
+                    break    
+            }
         }
     } 
 
-    for (BAttr in BAttrs) {
-    	if (AAttr !==  KEY && AAttr !== ON && truthy(BAttrs[BAttr])) { 
-    		D.setAttribute(BAttr, BAttrs[AAttr])
-    	}
-    }
-}
+    for (a in nextAttrs) { // set all those in B to A
 
+        currentVal = currentAttrs[a]
+        nextVal = nextAttrs[a]
 
+        if (!(nextVal === undefined || nextVal === null || nextVal === false) && 
+            nextVal !== currentVal &&
+            typeof nextVal !== FUNCTION) {
 
-// Node -> Attrs -> Attrs -> _
-// A more involved attribute updater that can deal with form fields
-function updateFieldAttributes (D, AAttrs, BAttrs) {
-
-    var AAttr, BAttr, BVal
-
-    for (AAttr in AAttrs) {
-
-    	BVal = BAttrs[AAttr]
-
-        if (!truthy(BVal)) {
-
-        	switch (AAttr) {
-                case KEY:
+            switch (a) {
                 case ON:
+                case KEY:
+                    break  
+                case CHECKED:
+                case DISABLED:
+                case SELECTED:
+                case VALUE:
+                    D[a] = nextVal
+                    break 
+                case FOCUS:
+                    D.focus()
                     break
-        		case VALUE:
-        			D.value = ''
+                default:
+                    D.setAttribute(a, nextVal)
                     break
-        		case CHECKED:
-        			D.checked = false
-                    break
-        		case SELECTED:
-        			D.selected = false
-                    break
-        		case FOCUS:
-        			D.blur()
-                    break
-        		default:
-        			D.removeAttribute(AAttr)
-                    break    
-        	}
+            }
         }
-    } 
-
-    for (BAttr in BAttrs) { // set all those in B to A
-
-    	BVal = BAttrs[BAttr]
-
-    	if (truthy(BVal)) {
-
-	    	switch (BAttr) {
-                case KEY:
-                case ON:
-                    break
-	    		case VALUE:
-	    			D.value = BVal
-                    break    
-	    		case CHECKED:
-	    			D.checked = !!BVal
-                    break
-	    		case SELECTED:
-	    			D.selected = !!BVal
-                    break
-	    		case FOCUS:
-	    			D.focus()
-                    break
-	    		default:
-	    			D.setAttribute(BAttr, BVal)
-                    break
-	    	}
-	    }
     }
-}
 
+    // update event listeners
+    if (currentEvts && !nextEvts) {
+        for (evt in currentEvts) {
+             D.removeEventListener(evt, currentEvts[evt])
+        }
 
+    } else if (!currentEvts && nextEvts) {
+        for (evt in nextEvts) {
+             D.addEventListener(evt, nextEvts[evt])
+        }
 
-
-// Node -> [Node] -> [VDom] -> [VDom] -> _
-// Updates the children in the Dom with a naive strategy
-// This is the default strategy
-function updateChildrenPairwise (D, DChildren, AChildren, BChildren) {
-
-    var d, a, b,
-        existsD, existsA, existsB,
-        i,
-        aType, bType
-
-    for (i = 0; i < Math.max(AChildren.length, BChildren.length); i++) {
-
-        d = DChildren[i]
-        a = AChildren[i]
-        b = BChildren[i]
-        existsD = typeof d !== UNDEFINED
-        existsA = typeof a !== UNDEFINED
-        existsB = typeof b !== UNDEFINED
-        
-        if (!existsD && !existsA && existsB) { // no d/a, but b
-            D.appendChild(vDomToDom(b))
-
-        } else if (existsD && existsA && !existsB) { // d/a, but no b
-            D.removeChild(d)
-
-        } else if (existsD && existsA && existsB) { // both
-
-            aType = getType(a)
-            bType = getType(b)
-        	
-            if (aType === bType && (aType === VNODE || aType === VCHILD) ||
-               (aType === VRENDERED && bType === VCHILD && a[1][0] === b[0])) {
-	                
-	            updateDom(d, a, b) // same tag or function, explore
-
-            } else if (a !== b) {
-                D.replaceChild(vDomToDom(b), d) // throwaway and regen
+    } else if (currentEvts && nextEvts) {
+        for (evt in currentEvts) {
+            if (!nextEvts[evt]) { 
+                D.removeEventListener(evt, currentEvts[evt]) 
+            }
+        }
+        for (evt in nextEvts) {
+            if (!currentEvts[evt] && currentEvts[evt] !== nextEvts[evt]) { 
+                D.addEventListener(evt, nextEvts[evt]) 
             }
         }
     }
 }
 
 
-// VDom -> [Node] -> [VDom] -> [VDom] -> _
-// Updates the children in the Dom with a hash-based, keyed strategy
-// This is used if the children have unique _key properties
-// The _key properties must faithfully represent the entire state of the 
-// child and all its attributes/children
-function updateChildrenKeyed (D, DChildren, AChildren, BChildren) {
-
-    var d, a, b, i
-    var AKeys = {} // { id: Node }
-    var AKey
-    var BKeys = {} // { id: vDom }
-    var BKey
-    var ANode
-
-    for (i = 0; i < Math.max(AChildren.length, BChildren.length); i++) {
-
-        d = DChildren[i]
-        a = AChildren[i]
-        b = BChildren[i]
-
-        if (truthy(a)) AKeys[a[1][KEY]] = { dom: d, vDom: a }
-        if (truthy(b)) BKeys[b[1][KEY]] = b
-    }
-
-    for (AKey in AKeys) {
-        if (BKeys[AKey] === undefined) {
-            D.removeChild(AKeys[AKey].dom)
-        }
-    } 
-
-    for (i = 0; i < BChildren.length; i++) {
-
-        b = BChildren[i]
-        ANode = AKeys[b[1][KEY]]
-
-        if (ANode === undefined) {
-            insertBeforeIndex(D, i, vDomToDom(b))
-
-        } else {
-            updateDom(ANode.dom, ANode.vDom, b) // assume they're the same type if they have the same id
-        }
-    }
-}
-
-
-
-
-// ---------- Virtual dom / dom / string transformers
-
-
-// HtmlString -> Node
-function htmlStringToDom (str) {
-    var el = document.createElement('div')
-    el.innerHTML = str
-    return el.firstChild
+// vDom -> vDom -> vType -> vType -> bool
+function shouldExploreFurther (current, next, currentType, nextType) {
+    return currentType === nextType &&
+            (currentType === VNODE || currentType === VCHILD) &&
+            current[0] === next[0]
 }
 
 
@@ -422,10 +281,7 @@ function vDomToHtmlString (vDom) {
     switch (getType(vDom)) {
 
         case VCHILD:
-            renderChild(vDom)
-            return vNodeToHtmlString(vDom[2])
-
-        case VRENDERED:
+            vDom[2] = vDom[0](vDom[1])
             return vNodeToHtmlString(vDom[2])
 
         case VNODE:
@@ -439,20 +295,22 @@ function vDomToHtmlString (vDom) {
 }
 
 
-// vNode -> HtmlString
-function vNodeToHtmlString (v) {
+// vDom -> HtmlString
+function vNodeToHtmlString (vDom) {
 
-    var tag = v[0]
-    var attrs = v[1]
-    var children = v[2]
+    var tag = vDom[0]
+    var attrs = vDom[1]
+    var val
+    var children = vDom[2]
     var a
     var attrPairs = []
     var c
     var res
 
     for (a in attrs) {
-        if (truthy(attrs[a]) && a !== KEY && a !== ON) { 
-            attrPairs.push(a + '="' + attrs[a] + '"') 
+        val = attrs[a]
+        if (!(val === undefined || val === null || val === false) && a !== KEY && a !== ON) { 
+            attrPairs.push(a + '="' + val + '"') 
         }
     }
 
@@ -460,66 +318,70 @@ function vNodeToHtmlString (v) {
 
     if (!VOID_ELEMENTS[tag]) {
         for (c = 0; c < children.length; c++) { 
-            res += vDomToHtmlString(children[c]) 
+            if (!(c === undefined || c === null || c === false)) {
+                res += vDomToHtmlString(children[c]) 
+            }
         }
         res += '</' + tag + '>'
     }
 }
 
 
-// vDom -> Node
+// vDom -> domNode
 function vDomToDom (vDom) {
-    const dom = htmlStringToDom(vDomToHtmlString(vDom))
-    bindInitialEvents(dom, vDom)
-    return dom
-}
 
+    switch (getType(vDom)) {
 
-function bindEvents (D, evts) {
-    for (var evt in evts) {
-        D.addEventListener(evt, evts[evt])
+        case VATOM:
+            return document.createTextNode(vDom)
+
+        case VNODE:
+        case VCHILD: // child rendering is handled by vDomToHtmlString
+            var el = document.createElement('div')
+            el.innerHTML = vDomToHtmlString(vDom)
+            var dom = el.firstChild
+            bindEvents(vDom, dom)
+            return dom
+
+        case VNULL:
+            throw new Error('Null vdom')
     }
 }
 
 
-function bindInitialEvents (D, vDom) {
 
-    var v = vDom
-    var dChildren
-    var i
+// vDom -> domNode -> _
+function bindEvents (vDom, D) {
 
-    if (vDom instanceof Array && D) {
+    var vType = getType(vDom)
+    var vNode
+    var evts
+    var evt
+    var child
+    var children
 
-        dChildren = slice.call(D.childNodes)
+    if (vType === VATOM || vType === VNULL) { return }
 
-        switch (getType(vDom)) {
+    vNode = vType === VCHILD ? vDom[2] : vDom
+    evts = vNode[1][ON]
+    children = vNode[2]
 
-            case VCHILD:
-                renderChild(vDom)
-                v = vDom[2]
-                break
-
-            case VRENDERED:
-                v = vDom[2]
-                break
+    if (evts) {
+        for (evt in evts) {
+             D.addEventListener(evt, evts[evt])
         }
-        
-        if (v[1] && v[1].on) {
-            bindEvents(D, v[1].on) 
-        }
+    }
 
-        if (v[2]) { 
-            for (i = 0; i < v[2].length; i++) {
-                bindInitialEvents(dChildren[i], v[2][i])
-            }
+    if (children) {
+        for (child = 0; child < children.length; child++) {
+            bindEvents(children[child], D.childNodes[child])
         }
     }
 }
-
-
 
 
 module.exports = {
+    vDomToHtmlString: vDomToHtmlString,
     vDomToDom: vDomToDom,
     updateDom: updateDom
 }
